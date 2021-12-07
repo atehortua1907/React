209e4c5a83e0b902791142d1cbc7bb771536f3df
David Atehortua <d.atehortua@sana-commerce.com>
2021-12-07 10:28:05 -0500

    Hooks, useState

--- 2-FernandoHerrera/2-Code/03-counter-app/src/CounterApp.js
+++ 2-FernandoHerrera/2-Code/03-counter-app/src/CounterApp.js
@@ -1,14 +1,22 @@
+import React, {useState} from 'react'
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
 
+
+    const [counter, setCounter] =  useState(0);    
+
     //handleAdd
-    const handleAdd = (e)=>{console.log(e)}
+    const handleAdd = (e)=>{
+        setCounter(counter+1);
+        //setCounter((c) => c + 1); //Cuando no tengamos acceso a la constante
+        // el hook retorna la variable que se recupera por paremetro
+    }
 
     return(
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
 
             <button onClick={handleAdd}>+1</button>
         </>

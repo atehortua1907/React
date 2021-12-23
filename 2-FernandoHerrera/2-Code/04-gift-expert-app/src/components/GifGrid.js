725dd72ee6ffa75b829c1bdb94e56232fffc39c9
David Atehortua <d.atehortua@sana-commerce.com>
2021-12-23 08:02:52 -0500

    helpers - getGifs

--- 2-FernandoHerrera/2-Code/04-gift-expert-app/src/components/GifGrid.js
+++ 2-FernandoHerrera/2-Code/04-gift-expert-app/src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
@@ -6,40 +7,27 @@ export const GifGrid = ({category}) => {
     const [images, setImages] = useState([])
 
     useEffect(() => {
-        getGifs();
-    }, [])
+        getGifs(category)
+        .then(setImages);
+    }, [category])
 
-    const getGifs = async() => {
-        const url = 'https://api.giphy.com/v1/gifs/search?api_key=scG7dHaQjy1mgl955EWkoMZ0vRr9FHHz&q=dragon&limit=2';
-        const resp = await fetch(url);
-        const {data} = await resp.json();
 
-        const gifs = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        });
-
-        console.log(gifs);
-        setImages(gifs);
-
-    }
 
     return (
         <>
             <h3>{category}</h3>
-            <ol>
-                {
-                    images.map( img =>
-                        <GifGridItem
-                            key={img.id}
-                            {...img}
-                        />
-                    )
-                }
-            </ol>
+            <div className='card-grid'>            
+                <ol>
+                    {
+                        images.map( img =>
+                            <GifGridItem
+                                key={img.id}
+                                {...img}
+                            />
+                        )
+                    }
+                </ol>
+            </div>
         </>
     )
 }

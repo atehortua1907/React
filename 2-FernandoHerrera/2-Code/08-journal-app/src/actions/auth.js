8af783a412abc49a16a5359e2ac1c743e8919e2f
David Atehortua <d.atehortua@sana-commerce.com>
2022-02-06 02:30:59 -0500

    Firebase: startRegisterWithEmailPasswordName

--- 2-FernandoHerrera/2-Code/08-journal-app/src/actions/auth.js
+++ 2-FernandoHerrera/2-Code/08-journal-app/src/actions/auth.js
@@ -9,6 +9,20 @@ export const startLoginEmailPassword = (email, password) => {
     }
 }
 
+export const startRegisterWithEmailPasswordName = (email, password, name) => {
+    return(dispatch) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then( async({ user }) => {
+
+            await user.updateProfile({displayName: name}) //para actualizar el display name del usuario
+            dispatch(
+                login(user.uid, user.displayName)
+            )
+        })
+        .catch(error => {console.error(error);})
+    };
+};
+
 export const startGoogleLogin = () =>{
     return(dispatch) =>{
         firebase.auth().signInWithPopup( googleAuthProvider )
--- 2-FernandoHerrera/2-Code/08-journal-app/src/components/auth/RegisterScreen.js
+++ 2-FernandoHerrera/2-Code/08-journal-app/src/components/auth/RegisterScreen.js
@@ -4,6 +4,7 @@ import { useForm } from '../../hooks/useForm';
 import validator from 'validator';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeErrorAction, setErrorAction } from '../../actions/ui';
+import { startRegisterWithEmailPasswordName } from '../../actions/auth';
 
 export const RegisterScreen = () => {
 
@@ -23,7 +24,7 @@ export const RegisterScreen = () => {
         e.preventDefault();
         
         if (isFormValid()){
-            console.log('Formulario correcto');
+            dispatch(startRegisterWithEmailPasswordName(email, password, name));
         }
     }
 
